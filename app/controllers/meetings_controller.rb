8060ed48ec96c42354a73efce688add92840d018
Guy Yogev <guy@spectory.com>
2016-08-11 08:13:46 +0300

    followup action item

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,13 +1,21 @@
 class MeetingsController < ApplicationController
   def show
     last_meetings = Meeting.all.order(created_at: :desc).take(2)
-    @meeting = Meeting.last
-    @meeting = last_meetings.first
-    @previouse_meeting = last_meetings.second
+    case last_meetings.count
+    when 0
+      @meeting = Meeting.create!
+      @previouse_meeting = nil
+    when 1
+      @meeting = last_meetings.first
+      @previouse_meeting = nil
+    else
+      @meeting = last_meetings.first
+      @previouse_meeting = last_meetings.second
+    end
     @new_notes = Note.where(meeting: nil)
     @reviewed_notes = Note.where(meeting: @meeting)
-    @previous_action_items = ActionItem.where(meeting_id: @previouse_meeting.id)
-    @new_action_items = ActionItem.where(meeting_id: @meeting.id)
+    @previous_action_items = ActionItem.where(meeting: @previouse_meeting)
+    @new_action_items = ActionItem.where(meeting: @meeting)
   end
 
   def update
