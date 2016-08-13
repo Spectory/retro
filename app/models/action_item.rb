563e2940c71638f097638fd642af6e4ad2202769
Guy Yogev <guy@spectory.com>
2016-08-13 08:45:43 +0300

    action types

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -1,3 +1,6 @@
 class ActionItem < ApplicationRecord
+  enum action_type: { keep_doing: 0, stop_doing: 1, start_doing: 2 }
+
   belongs_to :meeting
+
 end
--- db/migrate/20160729213453_create_action_items.rb
+++ db/migrate/20160729213453_create_action_items.rb
@@ -2,6 +2,7 @@ class CreateActionItems < ActiveRecord::Migration[5.0]
   def change
     create_table :action_items do |t|
       t.string  :title
+      t.integer :action_type
       t.text    :body
       t.string  :assigned_to
       t.belongs_to :meeting
