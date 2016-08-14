fc62804bf601dc83059b295a10acf80a3ec076c5
Guy Yogev <guy@spectory.com>
2016-08-14 16:00:07 +0300

    action item validations

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -4,4 +4,11 @@ class ActionItem < ApplicationRecord
   belongs_to :meeting
   belongs_to :note
 
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :action_type, presence: true
+  validates :assigned_to, presence: true
+  validates :meeting_id, presence: true
+  validates :note_id, presence: true
+
 end
